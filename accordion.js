203dba4423460b2dda8ccc7dbce90c3dd4cf2421
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-04 23:43:07 +0900

    fin

--- accordion.js
+++ accordion.js
@@ -1,30 +1,37 @@
 (()=>{
-  const $elm = document.querySelector('#js-accordion');
-  const $trigger = $elm.getElementsByTagName('a');
 
-  const triggerLen = $trigger.length;
-  let index = 0;
-  while(index < triggerLen){
-      $trigger[index].addEventListener('click', (e) => clickHandler(e));
-      index++;
-  }
+  class Accordion{
+    constructor(obj){
+      const $elm = document.querySelector(obj.hookName);
+      const $trigger = $elm.getElementsByTagName(obj.tagName);
 
-
-  //クリックされたら実行される処理
-  const clickHandler = (e) => {
-    e.preventDefault();
-
-    const $target = e.currentTarget;
-
-    const $content = $target.nextElementSibling;
-    if($content.style.display === 'block'){
-        $content.style.display = 'none';
-    } else {
-        $content.style.display = 'block';
+      const triggerLen = $trigger.length;
+      let index = 0;
+      while(index < triggerLen){
+        $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
+        index++;
+      }
     }
-    
+
+    //クリックされたら実行される処理
+    clickHandler(e){
+        e.preventDefault();
+
+        const $target = e.currentTarget;
+        const $content = $target.nextElementSibling;
+        
+        if($content.style.display === 'block'){
+          $content.style.display = 'none';
+        } else {
+          $content.style.display = 'block';
+        }
+      }
   }
+
+  const fuckingAccordion = new Accordion({
+    hookName: '#js-faq',
+    tagName: 'p'
+  });
 
 })();
-
  
