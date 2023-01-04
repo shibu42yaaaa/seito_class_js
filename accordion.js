4616103821d4a07a81541536fe8e461aed8acd0d
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-04 23:11:50 +0900

    finish-1

--- accordion.js
+++ accordion.js
@@ -2,13 +2,21 @@
   const $elm = document.querySelector('#js-accordion');
   const $trigger = $elm.getElementsByTagName('a');
 
-  $trigger[0].addEventListener('click', (e) => clickHandler(e));
+  const triggerLen = $trigger.length;
+  let index = 0;
+  while(index < triggerLen){
+      $trigger[index].addEventListener('click', (e) => clickHandler(e));
+      index++;
+  }
+
 
   //クリックされたら実行される処理
   const clickHandler = (e) => {
     e.preventDefault();
 
-    const $content = $trigger[0].nextElementSibling;
+    const $target = e.currentTarget;
+
+    const $content = $target.nextElementSibling;
     if($content.style.display === 'block'){
         $content.style.display = 'none';
     } else {
